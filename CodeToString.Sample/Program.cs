79d6984f5ac2506c8432eecc196a7770e8284993
NeVeSpl <nevespl@gmail.com>
2023-10-19 21:41:42 +0200

    initial Proof of Concept

---
+++ CodeToString.Sample/Program.cs
@@ -0,0 +1,15 @@
+﻿using NSourceGenerators;
+
+namespace CodeToString.Sample
+{
+    partial class Program
+    {
+        [CodeToString]
+        static void Main(string[] args)
+        {
+            HelloFrom(CodeToStringRepo.GetText("CodeToString.Sample.Program.Main"));         
+        }
+            
+        static partial void HelloFrom(string name);         
+    }
+}
---
+++ CodeToString/CodeToStringRepo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace NSourceGenerators
+{
+    internal static class CodeToStringRepo
+    {
+        private static readonly Dictionary<string, string> map;
+
+
+        public static string GetText(string index)
+        {
+            return map[index]; 
+        }
+
+
+        static CodeToStringRepo()
+        {
+               
+        }
+    }
+}
